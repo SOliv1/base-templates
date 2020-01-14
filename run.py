e31e6219561194ff765add7be6fca57b9640135f
Sara Oliver <s.oliver1@icloud.com>
2020-01-14 13:15:02 +0000

    run.py not working

--- run.py
+++ run.py
@@ -4,9 +4,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 @app.route('/about')
 def about():
@@ -14,6 +16,19 @@ def about():
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
+
+
+@app.route('/about/<member_name>')
+def about_member(member_name):
+    member = {}
+    
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    
+    return render_template("member.html", member=member)
 
 
 @app.route('/contact')
@@ -26,6 +41,7 @@ def careers():
     return render_template("careers.html", page_title="Careers")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',
-            port=8000,
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
             debug=True)
+            
