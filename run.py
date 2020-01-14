61bf592bfd7b7ae6139f5000df05cefa0d337ed3
Sara Oliver <s.oliver1@icloud.com>
2020-01-14 18:27:00 +0000

    post

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -27,11 +27,11 @@ def about_member(member_name):
                 
     return render_template("member.html", member=member)
 
-@app.route('/contact')
-
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+         print(request.form)
     return render_template("contact.html", page_title="Contact")
-
 
 @app.route('/careers')
 def careers():
