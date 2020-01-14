65b2500be49eb4a34df9ad8d01ebd742e762dcc6
Sara Oliver <s.oliver1@icloud.com>
2020-01-14 14:47:17 +0000

    advanced routing fixed

--- run.py
+++ run.py
@@ -4,11 +4,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template("index.html")
-
 
 @app.route('/about')
 def about():
@@ -16,7 +14,6 @@ def about():
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
-
 
 @app.route('/about/<member_name>')
 def about_member(member_name):
@@ -27,11 +24,11 @@ def about_member(member_name):
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
-    
+                
     return render_template("member.html", member=member)
 
-
 @app.route('/contact')
+
 def contact():
     return render_template("contact.html", page_title="Contact")
 
@@ -42,6 +39,5 @@ def careers():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+            port=int(os.environ.get("PORT", 8000)),
             debug=True)
-            
