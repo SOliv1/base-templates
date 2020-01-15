27d92648c944ecb31de4905e4f39f10554857e01
Sara Oliver <s.oliver1@icloud.com>
2020-01-15 15:55:23 +0000

    providing feedback to user

--- run.py
+++ run.py
@@ -15,28 +15,28 @@ def about():
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
-
+    
 @app.route('/about/<member_name>')
 def about_member(member_name):
     member = {}
-    
+   
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
-                
+   
     return render_template("member.html", member=member)
 
 @app.route('/contact', methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        flash("Thanks {}, we have received your message!".format(
+        flash("Thanks {}, we have received your message".format(
             request.form["name"]
         ))
     return render_template("contact.html", page_title="Contact")
-
 @app.route('/careers')
+
 def careers():
     return render_template("careers.html", page_title="Careers")
 
